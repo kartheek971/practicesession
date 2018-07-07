6786b01e8328032f95bbef0a8068020d93cdb834
kartheek971 <kartheek.jalluri@gmail.com>
2018-07-07 19:06:31 +0530

    abc1

---
+++ module1.rb
@@ -0,0 +1,14 @@
+module Trig
+def Trig.sinfun(x)
+ puts (Math.sin(x))
+end
+def Trig.cosfun(x)
+puts (Math.cos(x))
+end
+end
+Trig.cosfun(0)
+Trig.sinfun(0)
+
+
+# practicesession
+# practicesession
